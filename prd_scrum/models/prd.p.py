e3abe89d4b1bdc4563dd67fd8727266ca974c8ae
marcuszxc <marcus.karlsson@vertel.se>
2025-10-22 14:58:37 +0000

    prd_scrum: Fixed missing inverse_field on One2many field

--- prd_scrum/models/prd.p.py
+++ prd_scrum/models/prd.p.py
@@ -12,6 +12,7 @@ class ProductRequirementDocument(models.Model):
     project_id = fields.Many2one(comodel_name='project.project',string="Project",help="In this project functions may have tasks") 
     scrum_us_ids = fields.One2many(
         comodel_name='project.scrum.us',
+        inverse_name="doc_id",
         compute='_compute_scrum_us_ids',
         string='User Stories',
         store=False
@@ -86,7 +87,8 @@ class FunctionTypes(models.Model):
 
 class ProjectUserStories(models.Model):
     _inherit = 'project.scrum.us'
-
+    
+    doc_id = fields.Many2one(comodel_name="prd.document")
     func_id = fields.Many2one(comodel_name='prd.function',string="Function",help="")
 
 class ProjectTask(models.Model):
