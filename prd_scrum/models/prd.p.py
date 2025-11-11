6ba98c76fbcfc7f528e3b51566f597919d66e700
marcuszxc <marcus.karlsson@vertel.se>
2025-11-11 15:51:36 +0000

    prd_scrum: Smart buttons on task now work :)  T/6521

--- prd_scrum/models/prd.p.py
+++ prd_scrum/models/prd.p.py
@@ -15,6 +15,16 @@ class ProductRequirementDocument(models.Model):
         string='User Stories',
         store=False
     )
+    scrum_us_count = fields.Integer(compute='_scrum_us_count', string="User Stories", store=False)
+    task_ids = fields.One2many(comodel_name='project.task',compute='_compute_task_ids',string="Tasks",help="")
+    task_count = fields.Integer(compute='_task_count', string="Tasks")
+
+    @api.depends("scrum_us_ids")
+    def _scrum_us_count(self):
+        for p in self:
+            p.scrum_us_count = 0
+            if p.scrum_us_ids:
+                p.scrum_us_count = len(p.scrum_us_ids)
 
     @api.depends('function_ids.scrum_us_ids')  
     def _compute_scrum_us_ids(self):
@@ -27,18 +37,11 @@ class ProductRequirementDocument(models.Model):
             else:
                 doc.scrum_us_ids = False
 
-    scrum_us_count = fields.Integer(compute = '_scrum_us_count', string="User Stories")
-    def _scrum_us_count(self):
-        for p in self:
-            p.scrum_us_count = len(p.scrum_us_ids)
-    task_ids = fields.One2many(comodel_name='project.task',compute='_compute_task_ids',string="Tasks",help="") 
-
     @api.depends('function_ids.task_id')  
     def _compute_task_ids(self):
         for doc in self:
             doc.task_ids = doc.function_ids.mapped('task_id')
 
-    task_count = fields.Integer(compute = '_task_count', string="Tasks")
     def _task_count(self):
         for p in self:
             p.task_count = len(p.task_ids)
--- prd_scrum/models/project.py
+++ prd_scrum/models/project.py
@@ -16,20 +16,39 @@ class ProjectTask(models.Model):
 
     func_ids = fields.One2many(comodel_name='prd.function',compute='_compute_func_ids',string="Function",help="")
     func_count = fields.Integer(compute="_compute_func_ids")
+    req_ids = fields.One2many(comodel_name="prd.requirement", compute="_compute_req_ids")
+    req_count = fields.Integer(compute="_compute_req_ids")
 
     def _compute_func_ids(self):
-        self.ensure_one()
-        func_ids = self.env["prd.function"].search([("task_id", "=", self.id)])
-        self.func_ids = func_ids
-        self.func_count = len(func_ids)
+        for rec in self:       
+            func_ids = rec.env["prd.function"].search([("task_id", "=", rec.id)])
+            rec.func_ids = func_ids
+            rec.func_count = len(func_ids)
+    
+    def _compute_req_ids(self):
+        for rec in self:
+            func_ids = rec.env["prd.function"].search([("task_id", "=", rec.id)])
+            req_task_ids = func_ids.mapped("requirement_ids")
+            req_ids = req_task_ids.mapped("req_id")
+            rec.req_ids = req_ids
+            rec.req_count = len(req_ids)
 
     def action_get_funcs(self):
         return {
                 'type': 'ir.actions.act_window',
-                'res_model': 'ir.attachment',
+                'res_model': 'prd.function',
                 'views': [(False,'list'),(False,'form')],
                 'target': 'current',
                 'domain': [("id", "in", self.func_ids.ids)]
+            }
+    
+    def action_get_reqs(self):
+        return {
+                'type': 'ir.actions.act_window',
+                'res_model': 'prd.requirement',
+                'views': [(False,'list'),(False,'form')],
+                'target': 'current',
+                'domain': [("id", "in", self.req_ids.ids)]
             }
 
 
