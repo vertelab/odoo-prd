d64a1e63db5c94166bf57ae0806f547b115f437a
iammiracle <miracleaayodele@gmail.com>
2026-03-09 14:08:22 +0100

    prd_export_import manifest dependencies

--- prd_export_import/__manifest__.p.py
+++ prd_export_import/__manifest__.p.py
@@ -3,7 +3,7 @@
     'version': '18.0.1.0.0',
     'category': 'prd',
     'summary': 'Export and import PRD easily',
-    'depends': ['website_slides'],
+    'depends': ['prd', 'prd_scrum'],
     'data': [
         'security/ir.model.access.csv',
         'views/export_import_wizard_views.xml',
