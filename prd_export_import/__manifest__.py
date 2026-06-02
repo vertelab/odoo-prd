e5dbc938f5c974feee57f35f8e435984100e5e74
Vertel AB <vertel@vertel.se>
2026-06-02 22:13:24 +0000

    [18.0] add AGPL-3 license / fix invisible parameter

--- prd_export_import/__manifest__.py
+++ prd_export_import/__manifest__.py
@@ -3,11 +3,12 @@
     'version': '18.0.1.0.0',
     'category': 'prd',
     'summary': 'Export and import PRD easily',
-    'depends': ['prd', 'prd_scrum'],
+    'depends': ['website_slides'],
     'data': [
         'security/ir.model.access.csv',
         'views/export_import_wizard_views.xml',
     ],
     'installable': True,
     'auto_install': False,
+    'license': 'AGPL-3',
 }
