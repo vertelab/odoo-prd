744623813d79d798012e4a7b2db902d75dc7aba3
vertelbot <vertelbot@vertel.se>
2026-08-05 07:09:02 +0000

    odoobranchpfile odoo-prd from 18.0. Do not p-file sync

--- prd_export_import/__manifest__.py
+++ prd_export_import/__manifest__.py
@@ -2,7 +2,6 @@
     'name': 'PRD Export Import',
     'version': '18.0.1.0.0',
     'category': 'prd',
-    'license': 'LGPL-3',
     'summary': 'Export and import PRD easily',
     'depends': ['prd', 'prd_scrum'],
     'data': [
